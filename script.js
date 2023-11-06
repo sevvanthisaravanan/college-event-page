69a5583bef0f51d569b51c57ea7c1edd4eee32a8
sevvanthisaravanan <149099594+sevvanthisaravanan@users.noreply.github.com>
2023-11-06 18:09:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript to handle the registration button
+document.getElementById('registerBtn').addEventListener('click', function() {
+    alert('Registration Successful!');
+    // You can add more logic here to handle registration process
+});
